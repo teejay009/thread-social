8472be2c6fca189a38d54871735d5e4666d47676
teejay009 <tajdjimoh@gmail.com>
2023-11-23 14:53:35 +0100

    update

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -5,7 +5,7 @@ const UserHeader = () => {
   return <VStack gap={4} alignItems={"start"}>
     <Flex justifyContent={"space-between"} w={"full"}>
         <Box>
-            <Text > </Text>
+            <Text >A </Text>
         </Box>
     </Flex>
   </VStack>
