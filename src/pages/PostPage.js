f7764ef8708be7b4a79255ebdd7dd2354c9573cf
teejay009 <tajdjimoh@gmail.com>
2023-11-27 15:18:39 +0100

    update

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,13 +1,31 @@
+import { Flex } from "@chakra-ui/react"
 import UserHeader from "../components/UserHeader"
 import UserPosts from "../components/UserPosts"
 
 const PostPage = () => {
   return (
     <>
-    <UserHeader />
-    <UserPosts />
-      
+    <Flex>
+      <Flex w={"full"} gap={3} alignItems={"center"}>
+        <Avatar src="/aliumusa.jpeg" name="Aliu Musa" size=
+        {"md"} />
+
+        
+
+        <Text fontSize={"sm"}>Aliu Musa</Text>
+        <Image src="/verified.png" h={4} w={4} ml={2} />
+        <Flex alignItems={"center"}
+
+        <Text fontSize={"sm"}>2days</Text>
+        <Image src="/verified.png" h={4} w={4} ml={2} />
+
+      </Flex>
+
+    </Flex>
     </>
+   
+      
+    
   )
 }
 
