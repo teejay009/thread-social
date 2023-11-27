0ee272c23456f8e32c896f155385137d2ac21bd3
teejay009 <tajdjimoh@gmail.com>
2023-11-27 10:04:41 +0100

    update

--- src/components/UserPosts.js
+++ src/components/UserPosts.js
@@ -5,7 +5,9 @@ import { Link } from 'react-router-dom'
 function UserPosts() {
   return (
     <link to={"/aliumusa/post/1"}>
-        <Flex></Flex>
+        <Flex>
+            
+        </Flex>
       
     </link>
   )
