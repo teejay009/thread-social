220da8f2e8d88f895a41b228bc232c84ce8ce4f4
teejay009 <tajdjimoh@gmail.com>
2023-11-27 14:21:51 +0100

    update

--- src/components/UserPosts.js
+++ src/components/UserPosts.js
@@ -1,43 +1,82 @@
-import { Avatar, Flex, Box, Image, Text, center } from "@chakra-ui/react";
+import { Avatar, Flex, Box, Image, Text } from "@chakra-ui/react";
 import { BsThreeDots } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import Actions from "./Actions";
+import { useState } from "react";
 
-const UserPosts = () => {
+const UserPosts = ({ likes, replies, postTitle, postImage }) => {
+  const [liked, setLiked] = useState(false);
   return (
     <Link to={"/aliumusa/post/1"}>
       <Flex gap={3} mb={4} py={5}>
         <Flex flexDir={"column"} alignItems={"center"}>
           <Avatar src="/post1.png" name="Mark Zuckerberg" size={"md"} />
-          <Box w={"1px"} h={"full"} bg={"gray"} my={2}></Box>
+          <Box w={"1px"} h={"full"} bg={"gray.light"} my={2}></Box>
           <Box position={"relative"} w={"full"}>
-          <Avatar src="/post1.png" name="Mark Zuckerberg" size={"xs"} position={"absolute"} top={0} left={"15px"} padding={"2px"} />
-          <Avatar src="/post1.png" name="Bell shmurda" size={"xs"} position={"absolute"}  bottom={0} right={"-5px"} padding={"2px"} />
-          <Avatar src="/post1.png" name="Danfo driver" size={"xs"} position={"absolute"}  bottom={0} left={"4px"} padding={"2px"} />
+            <Avatar
+              src="/post1.png"
+              name="Mark Zuckerberg"
+              size={"xs"}
+              position={"absolute"}
+              top={0}
+              left={"15px"}
+              padding={"2px"}
+            />
+            <Avatar
+              src="/post1.png"
+              name="Bell shmurda"
+              size={"xs"}
+              position={"absolute"}
+              bottom={0}
+              right={"-5px"}
+              padding={"2px"}
+            />
+            <Avatar
+              src="/post1.png"
+              name="Danfo driver"
+              size={"xs"}
+              position={"absolute"}
+              bottom={0}
+              left={"4px"}
+              padding={"2px"}
+            />
           </Box>
         </Flex>
-        <Flex flex={1} flexDir={"column"} w={"full"}>
-            <Flex>
+        <Flex flex={1} flexDir={"column"} gap={2}>
+          <Flex w={"full"} justifyContent={"space-between"}>
             <Flex alignItems={"center"} w={"full"}>
-            <Text>AliuMusa</Text>
-            <Image src="/verified.png" ml={1} w={4} h={4} />
-          </Flex>
-
-          <Flex alignItems={"center"} gap={4}>
-            <Text>1day</Text>
-                <BsThreeDots />
-           
-          </Flex>
-
+              <Text>AliuMusa</Text>
+              <Image src="/verified.png" ml={1} w={4} h={4} />
             </Flex>
-            <Text fontSize={"sm"}>Hello Thread!!!</Text>
-            <Box overflow={"hidden"} borderRadius={6} border={"1px solid"} borderColor={"gray"}>
 
-                <Image src="/aliumusa.jpeg" width={"full"} />
-            </Box>
-        <Flex></Flex>
+            <Flex alignItems={"center"} gap={4}>
+              <Text>1day</Text>
+              <BsThreeDots />
+            </Flex>
+          </Flex>
+          <Text fontSize={"sm"}> {postTitle}</Text>
+          <Box
+            overflow={"hidden"}
+            borderRadius={6}
+            border={"1px solid"}
+            borderColor={"gray.light"}
+          >
+            <Image src="/aliumusa.jpeg" />
+          </Box>
+          <Flex>
+            <Actions liked={liked} setLiked={setLiked} />
+          </Flex>
+          <Flex
+            gap={2}
+            color={"gray.light"}
+            fontSize={"sm"}
+            alignItems={"center"}
+          >
+            <Text>{replies} Replies</Text>
+            <Box w={0.5} h={0.5} borderRadius={"full"} bg={"green"}></Box>
+            <Text>{likes} likes</Text>
+          </Flex>
         </Flex>
-
-       
       </Flex>
     </Link>
   );
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,13 +1,40 @@
-import UserHeader from "../components/UserHeader";
-import UserPosts from "../components/UserPosts";
+import UserHeader from "../components/UserHeader"
+import UserPosts from "../components/UserPosts"
+
 
 const UserPage = () => {
-  return (
-    <>
-      <UserHeader />
-      <UserPosts />
-    </>
-  );
-};
+  return(
+<>
+  <UserHeader />
+  <UserPosts userAvatar={"https://bit.ly/tioluwani-kolawole"}
+  likes={200}
+  replies={50}
+  postImage={"/post1.png"}
+  postTitle={"Hello, eku ojo merin nile yii o "}
+  />
 
-export default UserPage;
+   <UserPosts userAvatar={"https://bit.ly/tioluwani-kolawole"}
+  likes={100}
+  replies={40}
+  postImage={"/post1.png"}
+  postTitle={"This is great"}
+  
+  />
+
+<UserPosts userAvatar={"https://bit.ly/tioluwani-kolawole"}
+  likes={80}
+  replies={20}
+  postImage={"/post1.png"}
+  postTitle={"wow! awesome"} 
+  
+  />
+
+ 
+  </>
+
+  )
+ 
+  
+}
+
+export default UserPage
