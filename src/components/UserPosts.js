9552fa63ea3d0b24a44cc8098efcdcbfff8bcc05
teejay009 <tajdjimoh@gmail.com>
2023-11-27 10:06:14 +0100

    update

--- src/components/UserPosts.js
+++ src/components/UserPosts.js
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Avatar, Flex } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -6,7 +6,10 @@ function UserPosts() {
   return (
     <link to={"/aliumusa/post/1"}>
         <Flex>
-            
+            <Flex>
+                <Avatar />
+            </Flex>
+
         </Flex>
       
     </link>
