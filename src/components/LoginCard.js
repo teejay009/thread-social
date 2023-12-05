4643b801dd603eb8ee7d02ab0f08235e90e4a30d
teejay009 <tajdjimoh@gmail.com>
2023-12-05 11:13:13 +0100

    update

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -80,7 +80,10 @@ export default function LoginCard() {
               <Input
                 type="text"
                 onChange={(e) =>
-                  setInputs({ ...inputs, username: e.target.value })
+                  setInputs((inputs) => ({
+                    ...inputs, username: e.
+                      target.value
+                  }))
                 }
                 value={inputs.username}
               />
@@ -91,7 +94,10 @@ export default function LoginCard() {
                 <Input
                   type={showPassword ? "text" : "password"}
                   onChange={(e) =>
-                    setInputs({ ...inputs, password: e.target.value })
+                    setInputs((inputs) => ({
+                      ...inputs, password: e.
+                        target.value
+                    }))
                   }
                   value={inputs.password}
                 />
